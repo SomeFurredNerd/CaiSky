9a4bf472a5c9aaa23eb642cdf80fcd219d86479a
Cairo <somefurrednerd2@gmail.com>
2025-02-05 17:56:11 +1000

    added a more secure password storing method
    this method is currently only for windows, i don't really have a way to test linux right now and don't know how environment variables work on linux platforms so for now its windows only 
    
    sorry to all the linux femboys i'm working on it

--- bskypost.py
+++ bskypost.py
@@ -1,11 +1,24 @@
 from atproto import Client
+import os, base64
 
+osplatform = os.name
 
 bskypost = open("bskypost.txt", "r")
-username = open("username.txt", "r")
-password = open("password.txt", "r")
+
+if osplatform == "nt":
+    user64 = os.environ.get("csuser")
+    pass64 = os.environ.get("cspass")
+    username = base64.b64decode(user64).decode("utf-8")
+    password = base64.b64decode(pass64).decode("utf-8")
+else:
+    username = open("username.txt", "r")
+    password = open("password.txt", "r")
 
 client = Client()
-client.login(f'{username.read()}', f'{password.read()}')
+if osplatform == "nt":
+
+    client.login(username, password)
+else: 
+    client.login(f'{username.read()}', f'{password.read()}')
 
 post = client.send_post(bskypost.read())
--- login.py
+++ login.py
@@ -1,9 +1,26 @@
 from atproto import Client
+import os, base64
 
-username = open("username.txt", "r")
-password = open("password.txt", "r")
+osplatform = os.name
+
+if osplatform == "nt":
+    user64 = os.environ.get("csuser")
+    pass64 = os.environ.get("cspass")
+    username = base64.b64decode(user64).decode("utf-8")
+    password = base64.b64decode(pass64).decode("utf-8")
+else:
+    username = open("username.txt", "r")
+    password = open("password.txt", "r")
+
+
+# with open("debug.txt", "w") as file:
+#    file.write(f"{user64}, {pass64}, {username}, {password}") 
+
+
 
 client = Client()
-client.login(f'{username.read()}', f'{password.read()}')
 
-# help = input()
+if osplatform == "nt":
+    client.login(username, password)
+else: 
+    client.login(f'{username.read()}', f'{password.read()}')
